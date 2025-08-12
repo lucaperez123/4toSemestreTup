3d1903985d73d65e75a456bc7bf02d2cb7554355
Luca <107443874+lucaperez123@users.noreply.github.com>
2025-08-12 15:09:52 -0300

    carpeta js

---
+++ javascript/Eccomerce/client/js/products.js
@@ -0,0 +1,18 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Banana",
+        price: 480,
+        quanty: 1,
+        img: "/home/luca/4toSemestreTup/javascript/Eccomerce/client/media/fresh-ripe-banana-bunch-isolated-white-background-379900837.webp"
+    },
+
+    {
+        id: 2,
+        productName: "Leche",
+        price: 950,
+        quanty: 1,
+        img: "/"
+
+    }
+]
